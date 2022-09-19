98fa47926420756710f6ff8228e249e3cefb89cf
ErosHeavy666 <eros.garcia.arroyo@alumnos.upm.es>
2022-09-19 19:14:17 +0200

    Renombrado del top y de los paquetes Delete del sin_2 Restructura del generico register

--- Detailed_Design/Genericos/register.vhd
+++ Detailed_Design/Genericos/register.vhd
@@ -1,61 +1,55 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: Eros García Arroyo y Daniel Payno Zarceño
--- 
--- Create Date: 05.12.2018 12:46:40
--- Design Name: 
--- Module Name: register - Behavioral
--- Project Name: Sistema de grabación, tratamiento y reproducción de audio
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+----------------------------------
+-- Engineer: Eros Garcia Arroyo --
+----------------------------------
 
+---------------
+-- Libraries --
+---------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity reg is
-GENERIC(
-    d_width         :  INTEGER := 16);
-Port (
-        clk_12megas: in STD_LOGIC; --Entrada del reloj general del sistema de 12MHz
-        enable: in STD_LOGIC; --Enable que activa el FF de tipo D (se asociará con Sample_In_Enable)
-        reset: in STD_LOGIC; --Reset síncrono del sistema 
-        dato_in : in SIGNED(d_width-1 downto 0); --Datos de entrada -> Sample_In -> Xn
-        dato_out: out SIGNED(d_width-1 downto 0) --Datos de salida -> Xn -> Sample_Out
-        );
+------------
+-- Entity --
+------------
+entity register_d is
+  generic(
+    g_width : integer := 16);
+  port (
+    clk     : in std_logic; --Entrada del reloj general del sistema de 12MHz
+    n_reset : in std_logic; --Reset síncrono del sistema 
+    i_en    : in std_logic; --Enable que activa el FF de tipo D (se asociará con Sample_In_Enable)
+    i_data  : in std_logic_vector(g_width-1 downto 0); --Datos de entrada -> Sample_In -> Xn
+    o_data  : out std_logic_vector(g_width-1 downto 0) --Datos de salida -> Xn -> Sample_Out
+  );
 end reg;
 
+------------------
+-- Architecture --
+------------------
 architecture Behavioral of reg is
 
+  -- Signals
+  signal r_data, s_data : std_logic_vector (g_width-1 downto 0);
+  
 begin
-
-process(clk_12megas) --Proceso sensible a los flancos de subida del reloj general del sistema
-    begin
-        if(rising_edge(clk_12megas))  then
-            if(reset = '1') then --Si el reset esta a uno en un evento de subida del reloj
-                dato_out <= (others => '0'); --Se escribe un "00000000" a la salida del registro
-            elsif (enable = '1') then --Si el reset no esta activado y se detecta que enable esta a uno en
-                dato_out <= dato_in;  --un evento de subida del reloj, este coloca en la salida 
-            end if;                   --el valor de la entrada
-        end if;
-    end process;  
+  
+  -- Register process:
+  process(clk) 
+  begin
+    if(rising_edge(clk)) then
+      if(n_reset = '0') then       --Si el reset esta a cero en un evento de subida del reloj
+        r_data <= (others => '0'); --Se escribe un x"0000" a la salida del registro
+      elsif (i_en = '1') then      --Si el reset no esta activado y se detecta que enable esta a uno en
+        r_data <= s_data;          --un evento de subida del reloj, este coloca en la salida 
+      end if;                      --el valor de la entrada
+    end if;
+  end process;  
+  
+  -- Combinational logic process:
+  s_data <= i_data;
+  
+  -- Output process:
+  o_data <= r_data;
+  
 end Behavioral;
