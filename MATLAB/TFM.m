39fb27487d9a62479f5ba27d1badfe066e2e71eb
ErosHeavy666 <eros.garcia.arroyo@alumnos.upm.es>
2022-10-25 18:16:04 +0200

    TFM Script modified para 10,12,14,16,18b

--- MATLAB/TFM.m
+++ MATLAB/TFM.m
@@ -1,15 +1,11 @@
 %% Carga de un fichero .wav en Matlab y creaci ?on de otro fichero con el formato que emplea el
 %testbench.
-[data, fs] = audioread('input_test.wav');
-file = fopen('sample_in.dat','w');
+[data, fs] = audioread('guitar_input.wav');
+file = fopen('guitar_sample_in.dat','w');
 fprintf(file, '%d\n', round(data.*65535));
 
 %% Carga y escucha de un fichero con el formato de salida del testbench. 
-vhdlout=load('sample_out.dat')/65535;
+vhdlout=load('guitar_sample_in.dat')/65535;
 sound(vhdlout);
 
-%% Guarda el audio procesado
-load handel.mat
-
-audiowrite('haha_bankfilterHPF.wav',vhdlout,fs);
-clear y Fs
+%% Gráficas para comparar la cuantificación de los formatos de 12b, 14b y 16b
