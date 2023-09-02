d412097d2a3c6684e271c36240290d251e08256e
ErosHeavy666 <eros.garcia.arroyo@alumnos.upm.es>
2023-09-02 19:45:40 +0200

    Añadido los componentes de FIFO sources

---
+++ PROJECT/PROJECT.srcs/sources_1/ip/fifo_generator_test/fifo_generator_test_stub.v
@@ -0,0 +1,27 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+// Date        : Sun Feb 26 21:02:34 2023
+// Host        : DESKTOP-K6HLBBJ running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top fifo_generator_test -prefix
+//               fifo_generator_test_ fifo_generator_test_stub.v
+// Design      : fifo_generator_test
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tcsg324-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "fifo_generator_v13_2_3,Vivado 2018.3" *)
+module fifo_generator_test(clk, srst, din, wr_en, rd_en, dout, full, empty)
+/* synthesis syn_black_box black_box_pad_pin="clk,srst,din[15:0],wr_en,rd_en,dout[15:0],full,empty" */;
+  input clk;
+  input srst;
+  input [15:0]din;
+  input wr_en;
+  input rd_en;
+  output [15:0]dout;
+  output full;
+  output empty;
+endmodule
